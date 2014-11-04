ae89c8c2b1b22aada4e238c01d2e75e50e849ec2
Philipp Blume <pgbluster@gmail.com>
2014-11-04 03:18:38 -0600

    bare bones

---
+++ pinball-clone.js
@@ -0,0 +1,21 @@
+/*
+create playing area as tiled background, 12x12
+
+place "downward arrow" icons over [0][[1-2],[3-4],[5-6],[7-8],[9-10]]
+place "upward arrow" icons over [11][[1-2],[3-4],[5-6],[7-8],[9-10]]
+place "left arrow" icons over [[1-2],[3-4],[5-6],[7-8],[9-10]][0]
+place "right arrow" icons over [[1-2],[3-4],[5-6],[7-8],[9-10]][11]
+
+Give level-of-difficulty option (1-25)
+
+determine bumper configuration where (lvl-of-diff) bumpers are randomly placed on the grid
+
+
+show bumper configuration for 5 seconds
+
+show target
+
+await player's guess
+
+show bumpers and run player from guessed position.
+*/
